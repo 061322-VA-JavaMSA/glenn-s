be5a7cf20d56413ba137728fc78f3de833ee978b
Glenn Syham <glennsyham@gmail.com>
2022-06-14 23:05:21 -0700

    tuesday learning

---
+++ assignments/week1/my-first-eclipse-project/src/main/java/com/revature/scopes/Driver.java
@@ -0,0 +1,22 @@
+package com.revature.scopes;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass mc = new MyClass();
+		MyClass mc1 = new MyClass("Kevin");
+		
+		System.out.println(mc.reverseString("Nikita"));
+		System.out.println(mc.reverseString("Asmaa"));
+		
+		System.out.println("mc: " + mc.myString);
+		System.out.println("mc1: " + mc1.myString);
+		
+		boolean isCool; // not provided a default value
+		
+		int x = MyClass.myStaticVariable;
+		String s = mc.myString;
+	}
+
+}
