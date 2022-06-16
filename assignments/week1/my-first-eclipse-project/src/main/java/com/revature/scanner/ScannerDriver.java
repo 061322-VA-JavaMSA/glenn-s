acf8e428c5d258c6b51cb9f40dcdbb0076dea7dd
Glenn Syham <glennsyham@gmail.com>
2022-06-15 22:01:22 -0700

    updates copied files

--- assignments/week1/my-first-eclipse-project/src/main/java/com/revature/scanner/ScannerDriver.java
+++ assignments/week1/my-first-eclipse-project/src/main/java/com/revature/scanner/ScannerDriver.java
@@ -36,8 +36,8 @@ public class ScannerDriver {
 //		scan.close();
 //		loginScreen();
 		
-		menu();
-		//menuKevin();
+		//menu();
+		 menuKevin();
 	}
 	
 	public static void loginScreen() {
@@ -68,33 +68,68 @@ public class ScannerDriver {
 		 *  
 		 *  this menu should repeat until the user decides to exit
 		 */
-		String checkExit = "0";
-		while (!checkExit.equals("3"))  {
-			StringBuilder sb = new StringBuilder();
+		Boolean checkExit = true;
+		while (checkExit)  {
 			System.out.println("Menu please enter a number of your choice");
 			System.out.println("1: get a random number");
 			System.out.println("2: reverse a String");
 			System.out.println("3: exit the program");
 			String usernameInput = scan.nextLine();  
-			checkExit = usernameInput;
 			switch(usernameInput) {
 			case "1":
 				System.out.println("Your random number " +  Math.round(Math.random() * 100));
 			break;  	
 			case "2":
 				System.out.println("enter a text");
-				String textInput = scan.nextLine();  
-				sb.append(textInput);
+				StringBuffer sb = new StringBuffer(scan.nextLine());
 				System.out.println(sb.reverse());
 			break;  
 			case "3":
 				System.out.println("bye");
-			break;  			
+				checkExit = false;
+			break;  
+			default:
+				System.out.println("Invalid option.");			
  			}				
 		} 
 	
 		
 	}
 	
- 
+	public static void menuKevin() {
+		/*
+		 * Menu that gives a user multiple options:
+		 * 	- 1: get a random number
+		 *  - 2: reverse a String of the user's choice
+		 *  - 3: exit the program
+		 *  
+		 *  this menu should repeat until the user decides to exit
+		 */
+		
+		boolean run = true;
+		while(run) {
+			System.out.println("Please select an option:\n"
+								+ "1: to retrieve a random number\n"
+								+ "2: to reverse a word\n"
+								+ "3: to exit the program");
+			String input = scan.nextLine();
+			switch(input) {
+			case "1": 
+				System.out.println("Here's a random number: " + Math.round(Math.random() * 100));
+				break;
+			case "2":
+				System.out.println("Please enter a word:");
+				StringBuffer sb = new StringBuffer(scan.nextLine());
+				System.out.println("Here is your reversed word: " + sb.reverse());
+				break;
+			case "3":
+				run = false;
+				System.out.println("Goodbye.");
+				break;
+			default:
+				System.out.println("Invalid option.");
+			}
+			
+		}
+	} 
 }
