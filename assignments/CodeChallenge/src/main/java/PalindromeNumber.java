5180e02ce7ab5671336120342f0f03850834d516
Glenn Syham <glennsyham@gmail.com>
2022-07-05 06:44:21 -0700

    leet

--- assignments/CodeChallenge/src/main/java/PalindromeNumber.java
+++ assignments/CodeChallenge/src/main/java/PalindromeNumber.java
@@ -16,3 +16,22 @@ public class PalindromeNumber {
 	}
 
 }
+class PalindromeNumberSolution {
+    public boolean isPalindrome(int x) {
+		String st = (new Integer(x).toString());
+		String[] num_str =   st.split("");
+		boolean bl = false;
+		String st_outputString = "";
+		int check_zero = 0;
+		if(num_str[0].equals(num_str[(num_str.length - 1)])) {
+			for(int i=(num_str.length-1);i>=0;i--) {
+				st_outputString = st_outputString + num_str[i];
+			}
+			if( (Integer.parseInt(st_outputString) - x)  == 0 ) {
+				bl = true;
+			}
+		} 
+        
+        return bl;
+    }
+}
