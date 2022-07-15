a5bbae7978d34a3dc83a9d4a752673783457d311
Glenn Syham <glennsyham@gmail.com>
2022-07-15 03:30:23 -0700

    db.sql password was changed to text

--- projects/project1/db.sql
+++ projects/project1/db.sql
@@ -8,7 +8,7 @@ drop table if exists users;
 CREATE table if not exists  users (
 id SERIAL  PRIMARY KEY,
 username varchar(30)  not null,
-password varchar(30)  not null,
+password text  not null,
 first_name varchar(100)  not null,
 last_name varchar(100)  not null,
 email varchar(150)  not null,
